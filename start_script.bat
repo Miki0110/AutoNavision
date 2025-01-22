73faef443c7f995fd190948a319b83697316fa8f
Miki Pregaard <Mikipregaard@yahoo.dk>
2025-01-22 17:53:38 +0100

    minor change to the start script

--- start_script.bat
+++ start_script.bat
@@ -1,5 +1,15 @@
 @echo off
 cd /d "%~dp0"
 cd src
+
 call conda activate AutoNavision_env
+IF ERRORLEVEL 1 (
+    powershell -Command "[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') | Out-Null; [System.Windows.Forms.MessageBox]::Show('Error activating conda environment.');"
+    exit /b 1
+)
+
 python main.py
+IF ERRORLEVEL 1 (
+    powershell -Command "[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') | Out-Null; [System.Windows.Forms.MessageBox]::Show('Error running the Python script.');"
+    exit /b 1
+)
